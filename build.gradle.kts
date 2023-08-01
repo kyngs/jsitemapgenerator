1cf95c9d4645004c34c07b11ba56f207c41ef2e0
kyngs <kyngs@users.noreply.github.com>
2023-08-01 14:12:08 +0200

    Edit publish task, add CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,12 +27,27 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 
 java {
     withSourcesJar()
-    withJavadocJar()
 }
 
 publishing {
-    publications.create<MavenPublication>("maven") {
-        from(components["java"])
+    repositories {
+        maven {
+            name = "kyngsRepo"
+            url = uri(
+                    "https://repo.kyngs.xyz/" + (if (project.version.toString()
+                                    .contains("SNAPSHOT")
+                    ) "snapshots" else "releases") + "/"
+            )
+            credentials(PasswordCredentials::class)
+            authentication {
+                create<BasicAuthentication>("basic")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
     }
 }
 
